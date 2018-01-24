130e918c9bca8e68d64fefa182758eee2d599eb9
David Mack <david@sketchdeck.com>
2018-01-23 23:49:29 -0800

    Add dataset names

--- data_sets/synthetic_review_prediction/__init__.py
+++ data_sets/synthetic_review_prediction/__init__.py
@@ -14,3 +14,14 @@ runner.update(experiment_2_runner)
 runner.update(experiment_3_runner)
 runner.update(experiment_4_runner)
 runner.update(experiment_5_runner)
+
+
+from .experiment_1 import DATASET_NAME as EXPERIMENT_1_DATASET
+from .experiment_2 import DATASET_NAME as EXPERIMENT_2_DATASET
+from .experiment_3 import DATASET_NAME as EXPERIMENT_3_DATASET
+from .experiment_4 import DATASET_NAME as EXPERIMENT_4_DATASET
+from .experiment_5 import DATASET_NAME as EXPERIMENT_5_DATASET
+from .article_0    import DATASET_NAME as ARTICLE_0_DATASET
+
+
+
